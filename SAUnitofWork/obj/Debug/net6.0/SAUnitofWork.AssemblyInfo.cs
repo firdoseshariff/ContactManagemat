e99ce4d0f49a091e498e8e9bab78cb7b2cae814b
HaseebFawwaz <HaseebFawwaz@DESKTOP-JIDK12G>
2024-11-11 00:22:19 +0530

    commitnew

--- SAUnitofWork/obj/Debug/net6.0/SAUnitofWork.AssemblyInfo.cs
+++ SAUnitofWork/obj/Debug/net6.0/SAUnitofWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAUnitofWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb670fee0040478ea19a633467aa2cd9eaa579ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9d9931a6a13ae47dc68a7cf6796d3cf7efdba66")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAUnitofWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAUnitofWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
